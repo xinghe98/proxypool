0ad4454d277019028827ec07bd6989fbc7baa1f0
xinghe98 <lcg9899@gmail.com>
2022-09-01 17:20:00 +0800

    完善方法

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,23 @@
 import getProxyIp from "./get/getproxy";
 import testIp from "./detect/detect";
+import saveIp from "./save/saveIp";
+import express from "express";
+const app = express();
 const getIp = new getProxyIp();
 const testip = new testIp();
+const saveProxy = new saveIp();
 async function main() {
-  // setInterval(async () => {getIp.run();} ,50000)
-  setInterval(async () => {testip.run();} ,6000)
+  setInterval(async () => {getIp.run();} ,150000)
+  setInterval(async () => {testip.run();} ,60000)
+  app.get("/", async (req, res) => {
+    res.send("hello world");
+  })
+  app.get("/get", async (req, res) => {
+    res.send(await saveProxy.get());
+  })
 }
 // console.log(config.proxyurl);
 main();
-
+app.listen(3000, () => {
+  console.log("server is running on port 3000");
+})
--- src/save/saveIp.ts
+++ src/save/saveIp.ts
@@ -1,11 +1,11 @@
-import  Redis  from 'ioredis';
+import Redis from 'ioredis';
 import config from '../conf/config';
 // 储存模块
 class save {
     public client
     constructor() {
         this.client = new Redis(config.redis.url);
-        this.client.on('error', (err:any) => console.log('Redis Client Error', err));
+        this.client.on('error', (err: any) => console.log('Redis Client Error', err));
     }
     /**
      * 
@@ -18,15 +18,15 @@ class save {
      */
     async saveIp(value: string, score: number = 20): Promise<boolean> {
         try {
-            if (score<0) {
-            const originscore = await this.returnScore(value);
-            if (originscore===0){
-                await this.deleteip(value)
-                return true
-            }
-                await this.client.zadd("key", originscore+score, value)
-            }else{
-            await this.client.zadd("key", score, value)
+            if (score < 0) {
+                const originscore = await this.returnScore(value);
+                if (originscore === 0) {
+                    await this.deleteip(value)
+                    return true
+                }
+                await this.client.zadd("key", originscore + score, value)
+            } else {
+                await this.client.zadd("key", score, value)
             }
             return true;
         } catch (err) {
@@ -37,14 +37,14 @@ class save {
      * 
      * @returns Number(score)
      */
-    async returnScore(value:string) :Promise<number>{
-      const score =  await this.client.zscore("key",value);
-      return Number(score)
+    async returnScore(value: string): Promise<number> {
+        const score = await this.client.zscore("key", value);
+        return Number(score)
 
     }
     // 获取全部代理方法
-    async fetchIp(start:number,end:number): Promise<string[]> {
-        const re =await this.client.zrange("key",start,end);
+    async fetchIp(start: number, end: number): Promise<string[]> {
+        const re = await this.client.zrange("key", start, end);
         return re;
     }
     /**
@@ -52,9 +52,15 @@ class save {
      * @param member 
      * 当分数降为0时，删除该元素
      */
-    async deleteip(member:string){
-        await this.client.zrem("key",member)
+    async deleteip(member: string) {
+        await this.client.zrem("key", member)
     }
+    async get(): Promise<string> {
+        const porxy = await this.client.zrevrangebyscore("key", 100, 100)
+        // 从数组内随机取值
+        const random = Math.floor(Math.random() * porxy.length)
+        return porxy[random]
     }
+}
 
 export default save;
